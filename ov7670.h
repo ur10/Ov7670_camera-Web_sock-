b798f5f5e2ec151b7514505f50dacafdc7cb2ed3
UTKARSH RAI <37475612+ur10@users.noreply.github.com>
2019-08-25 20:26:44 +0530

    UPDATE

--- ov7670.h
+++ ov7670.h
@@ -1,11 +1,4 @@
-/*
- * This file is part of the OpenMV project.
- * Copyright (c) 2013/2014 Ibrahim Abdelkader <i.abdalkader@gmail.com>
- * This work is licensed under the MIT license, see the file LICENSE for details.
- *
- * OV7725 driver.
- *
- */
+
 #ifndef __OV7670_H__
 #define __OV7670_H__
 #include "sensor.h"
